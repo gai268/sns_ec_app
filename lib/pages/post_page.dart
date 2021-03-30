e7b694bcf1a9cb46ae89fb2187feed9dd3c57ef0
gai <tagai1106@gmail.com>
2021-03-30 21:18:35 +0900

    投稿ページに保存ボタンのデザインを追加

--- lib/pages/post_page.dart
+++ lib/pages/post_page.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:sns_ec_app/components/post_field.dart';
 
-class PostPage extends StatelessWidget{
+class PostPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(  // ヘッダーに表示するアプリケーションバー
-        title: Text("投稿"),  // タイトルを表示
+      appBar: AppBar(
+        // ヘッダーに表示するアプリケーションバー
+        title: Text("投稿"), // タイトルを表示
+        actions: <Widget>[
+          MaterialButton(
+            child: const Text('保存'),
+            shape: const StadiumBorder(),
+            onPressed: () {},
+          ),
+        ],
       ),
       body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            PostField(),
-          ],
-        )
-      ),
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          PostField(),
+        ],
+      )),
     );
   }
-
 }
